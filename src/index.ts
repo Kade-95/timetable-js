160443d98c5128d75c863a2de0093cac2c1659b4
Kade-95 <ikeka95@gmail.com>
2021-06-27 11:22:52 +0100

    Turn into library

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,48 @@
-import { days, periods, breakPeriods, groups } from "./../data/timetable.multi.json";
+import { ITimetable } from "./models/timetable.interface";
 import { Timetable } from "./utils/timetable.class";
-import * as Groups from './../data/groups.json';
-import { IGroup } from "./models/group.interface";
 
-
-function used(timeTable: Timetable) {    
-    const len = Groups.reduce((acc, reducer) => {
-        return acc + reducer.items.length;
-    }, 1);
-
-    const missing = timeTable.getMissingItems(Groups);
-    const used = len - missing.reduce((acc, reducer) => {
-        return acc + reducer.missing.length;
-    }, 1)
-
-    return (used / len) * 100 + '%';
+export {
+    Timetable,
+    ITimetable
 }
 
-const t = new Timetable(days, periods, breakPeriods, groups, 'single');
-// const aaagroups = JSON.parse(JSON.stringify(Groups))
-// const tgroup = t.getGroup('608e73327568794f9448e263')
+// import { days, periods, breakPeriods, groups } from "./../data/timetable.multi.json";
+// import * as Groups from './../data/groups.json';
+// import { IGroup } from "./models/group.interface";
 
-// const cgroup: IGroup = Groups.find(g => g._id == '608e73327568794f9448e263');
-// const items = cgroup.items.map(t => t._id);
-// const r = t.getGroupConflict('608e73327568794f9448e263', Groups)
+// function used(timeTable: Timetable) {    
+//     const len = Groups.reduce((acc, reducer) => {
+//         return acc + reducer.items.length;
+//     }, 1);
+
+//     const missing = timeTable.getMissingItems(Groups);
+//     const used = len - missing.reduce((acc, reducer) => {
+//         return acc + reducer.missing.length;
+//     }, 1)
+
+//     return (used / len) * 100 + '%';
+// }
+
+// const t = new Timetable(days, periods, breakPeriods, groups, 'single');
+// // const aaagroups = JSON.parse(JSON.stringify(Groups))
+// // const tgroup = t.getGroup('608e73327568794f9448e263')
+
+// // const cgroup: IGroup = Groups.find(g => g._id == '608e73327568794f9448e263');
+// // const items = cgroup.items.map(t => t._id);
+// // const r = t.getGroupConflict('608e73327568794f9448e263', Groups)
 
 
-const tu = used(t);
+// const tu = used(t);
 
-console.log(tu, t.getIssues(Groups), t.groups);
-const a = t.automateMultiple(Groups);
+// console.log(tu, t.getIssues(Groups), t.groups);
+// const a = t.automateMultiple(Groups);
 
-const au = used(a);
+// const au = used(a);
 
-console.log(au, a.getIssues(Groups), a.groups);
+// console.log(au, a.getIssues(Groups), a.groups);
 
-(window as any).groups = Groups;
-(window as any).timetable = t;
+// (window as any).groups = Groups;
+// (window as any).timetable = t;
 
 
 
