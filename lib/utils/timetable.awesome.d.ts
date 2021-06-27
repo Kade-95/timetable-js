818418cd63227d624ccb186da22dc0f5dd96d3a7
Kade-95 <ikeka95@gmail.com>
2021-06-27 15:32:47 +0100

    Commit for version 1.0.3

--- lib/utils/timetable.awesome.d.ts
+++
@@ -1,4 +0,0 @@
-import { IGroup } from "../models/group.interface";
-import { Timetable } from "./timetable.class";
-export declare function used(timeTable: Timetable, groups: IGroup[]): string;
-export declare function nully(timeTable: Timetable): string;
