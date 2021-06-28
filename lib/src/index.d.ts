73a48064f70eb4650e8afda9f987220c249162bb
Kade-95 <ikeka95@gmail.com>
2021-06-28 18:22:18 +0100

    Commit

---
+++ lib/src/index.d.ts
@@ -0,0 +1,4 @@
+import { ITimetable } from "./models/timetable.interface";
+import { TimetableManager } from "./utils/timetable.manager";
+import { IGroup } from './models/group.interface';
+export { TimetableManager, ITimetable, IGroup, };
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { ITimetable } from "./models/timetable.interface";
 import { TimetableManager } from "./utils/timetable.manager";
-
+import { IGroup } from './models/group.interface'
+// import {}
 export {
     TimetableManager,
-    ITimetable
+    ITimetable,
+    IGroup,
+    
 }
 
 // require('./test')
